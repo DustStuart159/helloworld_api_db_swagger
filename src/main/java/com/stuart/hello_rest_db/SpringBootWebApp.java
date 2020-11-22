d80bcbbb4c7fc300edd24c6fccff8f5b6a7911bd
DustStuart159 <ztrongnguyens96@gmail.com>
2020-11-22 13:15:34 +0700

    commit

--- src/main/java/com/stuart/hello_rest_db/SpringBootWebApp.java
+++ src/main/java/com/stuart/hello_rest_db/SpringBootWebApp.java
@@ -7,15 +7,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebApplication extends SpringBootServletInitializer {
+public class SpringBootWebApp extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootWebApplication.class);
+		return application.sources(SpringBootWebApp.class);
 	}
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootWebApplication.class, args);
+		SpringApplication.run(SpringBootWebApp.class, args);
 	}
 
 }
--- src/main/java/com/stuart/hello_rest_db/controllers/WelcomeController.java
+++ src/main/java/com/stuart/hello_rest_db/controllers/WelcomeController.java
@@ -13,13 +13,8 @@ import springfox.documentation.annotations.ApiIgnore;
 @ApiIgnore
 public class WelcomeController {
 
-	// inject via application.properties
-	@Value("${welcome.message:test}")
-	private String message = "Hello World";
-
 	@RequestMapping("/")
-	public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
+	public String welcome() {
 		return "index";
 	}
 
